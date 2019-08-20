efe64e2914189eca8531736c41259150e9a05c1b
dtanguy <pogonas91@hotmail.fr>
2019-08-21 01:15:00 +0200

    Update color.js

--- color.js
+++ color.js
@@ -15,3 +15,25 @@ console.error = function (message) {
 	let date = new Date(Date.now());
 	oriErr('\x1b[32m' , '[' , date.toTimeString().split(' ')[0] , ']' , '\x1b[31m' , " ==> " , message , '\x1b[0m');
 }
+
+
+function cyan(txt){
+	return '\x1b[36m' + txt + '\x1b[0m';
+}
+
+function green(txt){
+	return '\x1b[32m' + txt + '\x1b[0m';
+}
+
+function red(txt){
+	return '\x1b[31m' + txt + '\x1b[0m';
+}
+
+function magenta(txt){
+	return '\x1b[35m' + txt + '\x1b[0m';
+}
+
+module.exports.cyan = cyan;
+module.exports.green = green;
+module.exports.red = red;
+module.exports.magenta = magenta;
